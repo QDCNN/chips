1e7ea68f66831b6b7202e14030f8f276b5eb57e3
kokoro <8462304@qq.com>
2022-08-20 11:59:49 +0800

    fix: 修复上传

--- src/formily-components-vant/Uploader/index.tsx
+++ src/formily-components-vant/Uploader/index.tsx
@@ -1,17 +1,97 @@
+import { CommonApi } from '@/api';
+import { wxuuid } from '@/utils/uuid';
 import { Uploader as VantUploader} from '@antmjs/vantui'
 import { connect, mapProps } from '@formily/react'
+import Taro from '@tarojs/taro';
+import produce from 'immer';
+import { useEffect, useState } from 'react';
+
+const singleUpload = async (file, uploadParams) => {
+  const uuid = wxuuid();
+  const targetUrl = uploadParams.host + '/' + uuid;
+
+  await Taro.uploadFile({
+    url: uploadParams.host,
+    filePath: file.url,
+    name: 'file',
+    formData: {
+      key: uuid,
+      signature: uploadParams.signature,
+      policy: uploadParams.policy,
+      OSSAccessKeyId: uploadParams.accessKeyId,
+      'x-oss-security-token': uploadParams.securityToken
+    }
+  })
+  return targetUrl;
+}
+
+const handleAfterRead = async (files) => {
+  const aliossResponse = await CommonApi.getAliOSSInfo({});
+
+  return await Promise.all(files.map(file => singleUpload(file, aliossResponse.data)));
+}
+
+const onPreShowFileList = async (paths = [] as any[]) => {
+  const uuidList = paths.map(item => item.split('/').pop());
+  const response = await CommonApi.获取阿里云图片链接({ object: uuidList.join(',') });
+  const result = uuidList.map(item => response.data[item]).filter(item => item);
+  return result;
+};
+
+const transformToUploaderFileList = (fileList = [] as string[]) => {
+  return fileList.map(item => ({
+    url: item,
+    status: 'done',
+    message: '上传成功',
+    isImage: true
+  }));
+}
+
+const CustomVantUploader = (props) => {
+  const [showFileList, setShowFileList] = useState<any[]>([]);
+  const { fileList, onPreShowFileList, ...other } = props;
+
+  const handleFileListShow = async () => {
+    let nextFileList = fileList;
+    if (onPreShowFileList) nextFileList = await onPreShowFileList(fileList)
+    setShowFileList(transformToUploaderFileList(nextFileList));
+  }
+
+  useEffect(() => {
+    handleFileListShow();
+  }, [fileList]);
+
+  return (
+    <VantUploader fileList={showFileList} {...other} />
+  )
+}
 
 export const Uploader = connect(
-  VantUploader,
+  CustomVantUploader,
   mapProps(
     {
-      title: 'uploadText',
+      value: 'fileList',
     },
     (props, field: any) => {
+      const { onChange } = props;
+
       return {
-        afterRead: async (file) => {
-          console.log('file: ', file);
-        }
+        onAfterRead: async (e) => {
+          const { value = [] } = props;
+          const uploadImageList = await handleAfterRead(e.detail.file);
+          onChange([...value, ...uploadImageList]);
+        },
+        onDelete: (e) => {
+          const { fileList, index } = e.detail;
+          // const nextValue = e.detail.fileList;
+          // const index = e.detail.index;
+          // nextValue.splice()
+          const nextValue = produce(fileList, draft => {
+            draft.splice(index, 1);
+          });
+          onChange(nextValue);
+        },
+        onPreShowFileList,
       }
     }
   )
